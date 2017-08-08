b6e66687d78d4844515d77dd0cbab0e17af79d14
Mark Janes <mark.a.janes@intel.com>
2017-08-08 10:11:40 -0700

    Delete components
    To support multiple CI systems, automation is split into "Components"
    repo and "build_support" repo.  This mesa_ci repository will contain
    build_support and any automation common to all CI systems.

--- cts/build.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-
-import sys, os
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(sys.argv[0])), ".."))
-import build_support as bs
-
-bs.build(bs.CtsBuilder(suite="es"))
